bc139a0369fef26e1a20939b2a628deefa0b030e
pniyongabo <patrickniyongabo@gmail.com>
2022-01-19 20:41:24 -0800

    implemented isLeapYear method and added unit tests

--- leapdays/src/main/java/edu/pdx/cs410J/TeamOne/Kata.java
+++ leapdays/src/main/java/edu/pdx/cs410J/TeamOne/Kata.java
@@ -13,4 +13,17 @@ public class Kata {
     System.err.println("Missing command line arguments");
     System.exit(1);
   }
+
+  // @VisibleForTesting
+  static boolean isLeapYear(int year) {
+    if (year % 400 == 0) {
+      return true;
+    } else if (year % 100 == 0) {
+      return false;
+    } else if (year % 4 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- leapdays/src/test/java/edu/pdx/cs410J/TeamOne/KataTest.java
+++ leapdays/src/test/java/edu/pdx/cs410J/TeamOne/KataTest.java
@@ -2,6 +2,9 @@ package edu.pdx.cs410J.TeamOne;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class KataTest
 {
 
@@ -10,4 +13,28 @@ public class KataTest
     new Kata();
   }
 
+  @Test
+  void oddYearsAreNotLeapYears() {
+    assertThat(Kata.isLeapYear(2001), equalTo(false));
+    assertThat(Kata.isLeapYear(3333), equalTo(false));
+  }
+  
+  @Test
+  void yearDivisibleBy4ButNotBy100IsLeapYear() {
+    assertThat(Kata.isLeapYear(1996), equalTo(true));
+    assertThat(Kata.isLeapYear(4444), equalTo(true));
+  }
+
+
+  @Test
+  void yearDivisibleBy400IsLeapYear() {
+    assertThat(Kata.isLeapYear(2000), equalTo(true));
+    assertThat(Kata.isLeapYear(2400), equalTo(true));
+  }
+
+  @Test
+  void yearDivisibleBy100AndNot400IsNotLeapYear() {
+    assertThat(Kata.isLeapYear(2100), equalTo(false));
+    assertThat(Kata.isLeapYear(100), equalTo(false));
+  }
 }
